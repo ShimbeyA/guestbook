c6d0ff7db231614b31d8d9e84c72f701002c54f3
ShimbeyA <shimbeyassie@yahoo.com>
2016-01-28 11:51:25 -0800

    guestbook template view

--- guestbook/guestbook.js
+++ guestbook/guestbook.js
@@ -1,23 +1,52 @@
+Messages = new Mongo.Collection("messages");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  
+  Meteor.subscribe("messages");
+  
+  Template.guestBook.helpers({
+    "messages": function() {
+      return Messages.find( {}, {sort:{createdOn: -1}}) || {};
+    //return all messafe objects or an empty object if
+    //DB is invalid
     }
   });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  
+  Template.guestBook.events(
+      {
+        "submit form": function(event){
+          event.preventDefault();
+          
+          var messageBox =
+          $(event.target).find('textarea[name=guestBookMessage]');
+          
+          var messageText = messageBox.val();
+          
+          var nameBox=
+          $(event.target).find('input[name=guestName]');
+          var nameText= nameBox.val();
+          
+          Messages.insert(
+            {
+              name: nameText,
+              message: messageText,
+              createdOn: Date.now(),
+            });
+          nameBox.val("");
+          messageBox.val("");
+         // alert("Name is " + nameText + ", msg is " + messageText);
+      }
+      }      
+      );
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+  
+  Meteor.publish("messages", function() {
+    return Messages.find();
+  });
+  
 }
